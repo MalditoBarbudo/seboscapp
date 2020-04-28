d19fb2447a5002fb9b486866b3a75234a6d4ffef
MalditoBarbudo <victorgrandagarcia@gmail.com>
2020-04-28 11:17:23 +0200

    fixing forestime data creation. Added differences

--- data-raw/forestime_data.R
+++ data-raw/forestime_data.R
@@ -31,14 +31,16 @@ joined_data <-
     # coords_utm_x_ETRS89 = X,
     # coords_utm_y_ETRS89 = Y,
     old_idparcela = PLOTS,
-    CarbonoIFN23 = CarbonoIFN2,
-    CarbonoIFN34 = CarbonoIFN4,
+    CarbonoIFN3 = CarbonoIFN2,
+    CarbonoIFN4 = CarbonoIFN4,
     ErosIFN2 = Mitigada2,
     ErosIFN3 = Mitigada3,
     ErosIFN4 = Mitigada4,
     WaterIFN2 = Water2,
     WaterIFN3 = Water3,
     WaterIFN4 = Water4,
+    MaderaIFN3 = MaderaIFN23,
+    MaderaIFN4 = MaderaIFN34
   ) %>%
   left_join(
     ifn_plots_data,
@@ -72,14 +74,12 @@ all_together_data <-
     nfi = case_when(
       nfi == 'IFN2' ~ 'NFI_2',
       nfi == 'IFN3' ~ 'NFI_3',
-      nfi == 'IFN4' ~ 'NFI_4',
-      nfi == 'IFN23' ~ 'NFI_2_NFI_3',
-      nfi == 'IFN34' ~ 'NFI_3_NFI_4',
+      nfi == 'IFN4' ~ 'NFI_4'
     ),
     es_name = case_when(
       es_name == 'Water' ~ 'exported_water',
       es_name == 'Carbono' ~ 'soil_organic_carbon',
-      es_name == 'Eros' ~ 'erosion_control',
+      es_name == 'Eros' ~ 'erosion_mitigation',
       es_name == 'Setas' ~ 'mushrooms_production',
       es_name == 'Madera' ~ 'wood'
     )
@@ -108,15 +108,96 @@ nfi_4_data <-
   sf::st_as_sf(sf_column_name = 'geometry')
 
 nfi_2_nfi_3_data <-
-  all_together_data %>%
-  filter(nfi == 'NFI_2_NFI_3') %>%
-  pivot_wider(names_from = es_name, values_from = values) %>%
-  select(-nfi) %>%
+  nfi_3_data %>%
+  as_tibble() %>%
+  select(-geometry) %>%
+  left_join(
+    nfi_2_data %>% as_tibble() %>% select(-geometry),
+    by = c("plot_id", "admin_province", "admin_region", "admin_municipality"),
+    suffix = c('_nfi_3', '_nfi_2')
+  ) %>%
+  mutate(
+    soil_organic_carbon_nfi_2 = NA,
+    soil_organic_carbon_nfi_3 = soil_organic_carbon,
+    wood_nfi_2 = NA,
+    wood_nfi_3 = wood
+  ) %>%
+  select(-soil_organic_carbon, -wood) %>%
+  mutate(
+    soil_organic_carbon = soil_organic_carbon_nfi_3 - soil_organic_carbon_nfi_2,
+    erosion_mitigation = erosion_mitigation_nfi_3 - erosion_mitigation_nfi_2,
+    mushrooms_production = mushrooms_production_nfi_3 - mushrooms_production_nfi_2,
+    exported_water = exported_water_nfi_3 - exported_water_nfi_2,
+    wood = wood_nfi_3 - wood_nfi_2,
+  ) %>%
+  select(
+    plot_id, admin_province, admin_region, admin_municipality,
+    soil_organic_carbon, erosion_mitigation, mushrooms_production,
+    exported_water, wood
+  ) %>%
+  left_join(
+    nfi_2_data %>% select(plot_id, admin_province, admin_region, admin_municipality),
+    by = c("plot_id", "admin_province", "admin_region", "admin_municipality")
+  ) %>%
   sf::st_as_sf(sf_column_name = 'geometry')
 
 nfi_3_nfi_4_data <-
-  all_together_data %>%
-  filter(nfi == 'NFI_3_NFI_4') %>%
-  pivot_wider(names_from = es_name, values_from = values) %>%
-  select(-nfi) %>%
+  nfi_4_data %>%
+  as_tibble() %>%
+  select(-geometry) %>%
+  left_join(
+    nfi_3_data %>% as_tibble() %>% select(-geometry),
+    by = c("plot_id", "admin_province", "admin_region", "admin_municipality"),
+    suffix = c('_nfi_4', '_nfi_3')
+  ) %>%
+  mutate(
+    soil_organic_carbon = soil_organic_carbon_nfi_4 - soil_organic_carbon_nfi_3,
+    erosion_mitigation = erosion_mitigation_nfi_4 - erosion_mitigation_nfi_3,
+    mushrooms_production = mushrooms_production_nfi_4 - mushrooms_production_nfi_3,
+    exported_water = exported_water_nfi_4 - exported_water_nfi_3,
+    wood = wood_nfi_4 - wood_nfi_3,
+  ) %>%
+  select(
+    plot_id, admin_province, admin_region, admin_municipality,
+    soil_organic_carbon, erosion_mitigation, mushrooms_production,
+    exported_water, wood
+  ) %>%
+  left_join(
+    nfi_3_data %>% select(plot_id, admin_province, admin_region, admin_municipality),
+    by = c("plot_id", "admin_province", "admin_region", "admin_municipality")
+  ) %>%
+  sf::st_as_sf(sf_column_name = 'geometry')
+
+nfi_2_nfi_4_data <-
+  nfi_4_data %>%
+  as_tibble() %>%
+  select(-geometry) %>%
+  left_join(
+    nfi_2_data %>% as_tibble() %>% select(-geometry),
+    by = c("plot_id", "admin_province", "admin_region", "admin_municipality"),
+    suffix = c('_nfi_4', '_nfi_2')
+  ) %>%
+  mutate(
+    soil_organic_carbon_nfi_2 = nfi_3_data$soil_organic_carbon,
+    soil_organic_carbon_nfi_4 = soil_organic_carbon,
+    wood_nfi_2 = nfi_3_data$wood,
+    wood_nfi_4 = wood
+  ) %>%
+  select(-soil_organic_carbon, -wood) %>%
+  mutate(
+    soil_organic_carbon = soil_organic_carbon_nfi_4 - soil_organic_carbon_nfi_2,
+    erosion_mitigation = erosion_mitigation_nfi_4 - erosion_mitigation_nfi_2,
+    mushrooms_production = mushrooms_production_nfi_4 - mushrooms_production_nfi_2,
+    exported_water = exported_water_nfi_4 - exported_water_nfi_2,
+    wood = wood_nfi_4 - wood_nfi_2,
+  ) %>%
+  select(
+    plot_id, admin_province, admin_region, admin_municipality,
+    soil_organic_carbon, erosion_mitigation, mushrooms_production,
+    exported_water, wood
+  ) %>%
+  left_join(
+    nfi_2_data %>% select(plot_id, admin_province, admin_region, admin_municipality),
+    by = c("plot_id", "admin_province", "admin_region", "admin_municipality")
+  ) %>%
   sf::st_as_sf(sf_column_name = 'geometry')
